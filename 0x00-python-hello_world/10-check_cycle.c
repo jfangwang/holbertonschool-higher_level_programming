6e047ab12a50fa67ebbecbef510fc5cc06b8beb2
jfangwang <qbs1864@gmail.com>
2020-12-07 19:26:40 +0000

    10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,23 @@
+#include "list.h"
+/**
+ * check_cycle - checks if the linked list is a loop
+ * @willy - A struct pointing to a list
+ * Return: 0
+ */
+int check_cycle(listint_t *willy)
+{
+	listint_t *ptr_1 = willy;
+	/*Will increment by 1 */
+	listint_t *ptr_2 = willy;
+	/*Will increment by 2 */
+	if (list != NULL)
+		return (0);
+	while (ptr_2 && ptr_2->next)
+	{
+		ptr_1 = ptr_1->next;
+		ptr_2 = ptr_2->next->next;
+		if (ptr_1 == ptr_2)
+			return (1);
+	}
+	return (0)
+}
