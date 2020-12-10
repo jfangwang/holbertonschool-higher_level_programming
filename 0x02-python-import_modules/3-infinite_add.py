4f1b84c87b6644d13bc2095778e528ae77861536
jfangwang <qbs1864@gmail.com>
2020-12-10 18:52:23 +0000

    3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+from sys import argv
+if __name__ == "__main__":
+    total = 0
+    if len(argv) == 1:
+        print("0")
+    else:
+        for willy in range(1, len(argv)):
+            total += int(argv[willy])
+        print("{:d}".format(total))
