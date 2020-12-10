414e96c6d94d1b208aad67256d22f8b729f7c217
jfangwang <qbs1864@gmail.com>
2020-12-10 18:47:24 +0000

    2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+from sys import argv
+if __name__ == "__main__":
+    if len(argv) == 1:
+        print("0 arguments.")
+    else:
+        print("{:d} arguments:".format(len(argv) - 1))
+        for willy in range(1, len(argv)):
+            print("{:d}: {:s}".format(willy, argv[willy]))
