bf2a268f5e0b4649e8650e2bd670bad6aae6e69a
jfangwang <qbs1864@gmail.com>
2020-12-15 19:40:57 +0000

    6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -9,4 +9,4 @@ def print_matrix_integer(matrix=[[]]):
                     else:
                         print("{:d}".format(adam), end=' ')
                     count += 1
-        print()
+            print()
