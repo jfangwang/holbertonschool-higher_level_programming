5f1f7feb466221a5207d11a08a385461f1984010
jfangwang <qbs1864@gmail.com>
2020-12-15 20:05:12 +0000

    7 done

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,15 @@
+#!/bin/bash
+def add_tuple(tuple_a=(), tuple_b=()):
+    tuple_a = check(tuple_a)
+    tuple_b = check(tuple_b)
+    return ((tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1]))
+
+
+def check(tup=()):
+    if len(tup) == 1:
+        tup = (tup[0], 0)
+    elif len(tup) == 0:
+        tup = (0, 0)
+    elif len(tup) > 2:
+        tup = (tup[0], tup[1])
+    return tup
