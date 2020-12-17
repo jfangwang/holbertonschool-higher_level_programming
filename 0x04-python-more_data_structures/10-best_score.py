1fc96bca287aff180b2a0f649ccbc19b2589ceb2
jfangwang <qbs1864@gmail.com>
2020-12-17 21:46:47 +0000

    10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     high = 0
+    name = ""
     if not a_dictionary:
         return None
     for a, b in a_dictionary.items():
         if b > high:
-            high = b
-    return high
+            name = a
+    return name
