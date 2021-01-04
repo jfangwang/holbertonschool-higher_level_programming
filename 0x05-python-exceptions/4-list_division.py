27294dd92d5b1d5149ebd60640df331dfe7de02f
jfangwang <qbs1864@gmail.com>
2021-01-04 18:26:22 +0000

    added a custom error

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-    if len(my_list_1) != len(my_list_2):
-        raise IndexError
     output = []
     count = 0
 
-    try:
-        for a in my_list_1:
-            output[count] = my_list_1[count] / my_list_2[count]
+
+    for a in range(list_length):
+        try:
+            output.append(my_list_1[count] / my_list_2[count])
+        except IndexError:
+            output.append(0)
+            print("out of range")
+        except ZeroDivisionError:
+            output.append(0)
+            print("division by 0")
+        except TypeError:
+            output.append(0)
+            print("wrong type")
+        except:
+            print("error unknown")
+        finally:
             count += 1
-    except IndexError:
-        print("out of range")
-    except ZeroDivisionError:
-        print("division by 0")
-    except TypeError:
-        print("wrong type")
-    else:
-        print(output)
+    return output
