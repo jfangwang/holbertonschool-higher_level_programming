62a5a6d8a3f552dd43c1fbcfa78bce930cc0ef15
jfangwang <qbs1864@gmail.com>
2021-01-04 17:51:23 +0000

    format issue

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,10 +4,10 @@ def safe_print_list(my_list=[], x=0):
 
     try:
         for a in range(x):
-            print("{:d}".format(my_list[a]), end=' ')
+            print("{:d}".format(my_list[a]), end='')
             count += 1
-        print()
     except TypeError:
         pass
     finally:
+        print()
         return count
