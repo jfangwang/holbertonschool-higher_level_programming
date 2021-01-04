ce401d5ade10bfcc3c1ed5b5865c41f992af8539
jfangwang <qbs1864@gmail.com>
2021-01-04 17:52:07 +0000

    changed to false

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -4,4 +4,4 @@ def safe_print_integer(value):
         print("{:d}".format(value))
         return True
     except (IndexError, TypeError, ValueError):
-        return str(value).isdigit()
+        return False
