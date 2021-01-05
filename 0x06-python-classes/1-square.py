a5dbc5ce5c58b66b37b80a4d454ee6b2ad8974ed
jfangwang <qbs1864@gmail.com>
2021-01-05 21:58:44 +0000

    1 done

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Square Class"""
+
+
+class Square:
+
+    """__init__ method
+
+    The __init__ method intializes a size of the square
+
+    Note:
+        This is annoying and I am gonna have to learn this and I
+        understand why this is necessay.
+    Attributes:
+        size (int): A private value defining the square
+
+    """
+
+    def __init__(self, size):
+        self.__size = size
