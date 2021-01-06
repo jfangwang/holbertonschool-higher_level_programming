91f9ccdff1038b37293903ee0786806480eadfec
jfangwang <qbs1864@gmail.com>
2021-01-06 01:05:24 +0000

    fixing edge cases

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -23,7 +23,17 @@ class Square:
     """
 
     def __init__(self, size=0, position=(0, 0)):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
+        if type(value) is not tuple or value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif type(value[0]) is not int or type(value[1]) is not int:
+            raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = position
 
     """size
