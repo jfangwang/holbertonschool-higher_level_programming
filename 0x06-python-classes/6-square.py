592ec93e027c9cbb0293142d365241cd57f71fed
jfangwang <qbs1864@gmail.com>
2021-01-06 15:33:04 +0000

    include private in init

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -6,8 +6,8 @@ class Square:
     """This is a class"""
     def __init__(self, size=0, position=(0, 0)):
         """method"""
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
