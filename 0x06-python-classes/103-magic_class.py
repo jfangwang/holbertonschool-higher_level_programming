385442a4d8c6c9d4cdabe7d07ad02b3b84278dc4
jfangwang <qbs1864@gmail.com>
2021-01-06 15:36:47 +0000

    include private in init

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -4,21 +4,18 @@ import math
 
 
 class MagicClass:
-    """ Filler Comment
-    """
+    """Magic Class"""
     def __init__(self, radius):
+        """Initializing"""
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError('readius must be a number')
         self.__radius = radius
-        """
-        """
+
     def area(self):
+        """ Area """
         return self.__radius ** 2 * math.pi
-        """
-        """
+
     def circumference(self):
+        """ Circumference """
         return 2 * math.pi * self.__radius
-    dis.dis(__init__)
-    dis.dis(area)
-    dis.dis(circumference)
