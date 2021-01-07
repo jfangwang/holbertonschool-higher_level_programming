ba1e0e0cbd7937bf61d5310898d2d44f4bd9f8b3
jfangwang <qbs1864@gmail.com>
2021-01-07 03:46:01 +0000

    102

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+class Square:
+    """Class Square"""
+    def __init__(self, size=0):
+        """init"""
+        self.__size = size
+
+    @property
+    def size(self):
+        """Getter"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter"""
+        if type(value) is not int:
+            raise TypeError("size must be a number")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """area"""
+        return self.__size ** 2
+
+    def __lt__(self, value):
+        """Less Than"""
+        return self.size < value.size
+
+    def __le__(self, value):
+        """Less Than Equal"""
+        return self.size <= value.size
+
+    def __eq__(self, value):
+        """Equal"""
+        return self.size == value.size
+
+    def __ne__(self, value):
+        """Not Equal"""
+        return self.size != value.size
+
+    def __gt__(self, value):
+        """Greater Than"""
+        return self.size > value.size
+
+    def __ge__(self, value):
+        """Greater Than Equal"""
+        return self.size >= value.size
