99bc8f1523434144d64bea21088d3a53c31a46c9
jfangwang <qbs1864@gmail.com>
2021-01-08 20:14:40 +0000

    pep8

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -5,7 +5,7 @@ max_integer = __import__("6-max_integer").max_integer
 
 
 class TestMaxInt(unittest.TestCase):
-
+    """testMaxInt"""
     def test_import(self):
         """import"""
         self.assertTrue(__import__("6-max_integer").max_integer)
@@ -26,11 +26,11 @@ class TestMaxInt(unittest.TestCase):
         self.assertEqual(max_integer([-1.1, -2.2, -3.3, -4.69420]), -1.1)
         self.assertEqual(max_integer([-1.1, -2.2, -3.3, 4.69420]), 4.69420)
         self.assertEqual(max_integer([-1.1, -2.2, 3.3, -4.69420]), 3.3)
-    
+
     def test_empty(self):
         """Nothing"""
         self.assertRaises(TypeError, max_integer, None)
-    
+
     def test_NotList(self):
         self.assertRaises(TypeError, max_integer, 2)
 
