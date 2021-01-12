b52d32d9bd77a114d9b22be9c4dc59de6767e213
jfangwang <qbs1864@gmail.com>
2021-01-12 02:58:37 +0000

    3 is done

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,1 +1,60 @@
 #!/usr/bin/python3
+"""Class Rectangle"""
+
+
+class Rectangle:
+    """Reactangle Class"""
+    def __init__(self, width=0, height=0):
+        """Init"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Getter"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise TypeError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter"""
+        return self.__height
+
+    @width.setter
+    def height(self, value):
+        """Setter"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise TypeError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Area"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Perimeter"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """__str__"""
+        output = ""
+        if self.__width == 0 or self.__height == 0:
+            return output
+        for a in range(self.__height):
+            for b in range(self.__width):
+                output += "#"
+            if a < self.__height - 1:
+                output += '\n'
+        return output
