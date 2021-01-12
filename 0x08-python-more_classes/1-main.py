6e78cfc3dcf68aa1d0f64d6c90f32824882b6308
jfangwang <qbs1864@gmail.com>
2021-01-12 05:24:55 +0000

    1-fixed

--- 0x08-python-more_classes/1-main.py
+++ 0x08-python-more_classes/1-main.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
 Rectangle = __import__('1-rectangle').Rectangle
 
-try:
-    my_rectangle = Rectangle(2, -3)
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
-
-try:
-    my_rectangle = Rectangle(-2, 3)
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
+my_rectangle = Rectangle(2, 4)
+print("{} - {}".format(my_rectangle.width, my_rectangle.height))
+my_rectangle.width = 10
+print("{} - {}".format(my_rectangle.width, my_rectangle.height))
