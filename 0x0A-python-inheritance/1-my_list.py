4ef9e36168b93054148bcb3760b1d06c1736f260
jfangwang <qbs1864@gmail.com>
2021-01-19 18:41:01 +0000

    implemented unit tests

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Task 2"""
+
+
+class MyList(list):
+    """Sorting a list"""
+    def __init__(self):
+        """init"""
+        pass
+
+    def print_sorted(self):
+        """Sort List"""
+        print(sorted(self))
