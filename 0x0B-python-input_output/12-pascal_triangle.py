af70e21f1aef9816fc49a66a6f07c493d51712ab
jfangwang <qbs1864@gmail.com>
2021-01-21 02:57:31 +0000

    12

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -4,28 +4,31 @@
 
 def pascal_triangle(n):
     """pascal"""
-    row = 0
-    bucket1 = 0
-    triangle = []
     rowArr = []
-    count = 0
+    triangle = []
+    num1 = 0
+    num2 = 0
 
     if n <= 0:
         return triangle
-
     triangle = [[1]]
-
     if n == 1:
         return triangle
 
-    for a in range(1, n):
-        """n is greater than 2"""
-        count = 0
+    for a in range(2, n + 1):
+        """row"""
         rowArr = []
-        for index in range(0, a + 1):
-            rowArr.append(count)
-            print(count)
-            count += 1
+        num1 = 0
+        num2 = 0
+        for b in range(0, a):
+            """col"""
+            """prints the index of the previous row"""
+            if b < a - 1:
+                print(triangle[a - 2][b])
+                num1 = triangle[a - 2][b]
+                rowArr.append(num1 + num2)
+                num2 = num1
+            if b == a - 1:
+                rowArr.append(1)
         triangle.append(rowArr)
-    print(triangle)
     return triangle
