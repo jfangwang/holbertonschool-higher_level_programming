d2ac57fcd68c3e314e575bdc477c9e720169c9ea
jfangwang <qbs1864@gmail.com>
2021-01-21 04:08:52 +0000

    100 fixed

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -4,7 +4,7 @@
 
 def append_after(filename="", search_string="", new_string=""):
     """append after"""
-    with open(filename, 'r+', encoding='utf-8') as file:
+    with open(filename, 'r', encoding='utf-8') as file:
         content = file.readlines()
         lc = 0
         for index in range(0, len(content) - 1):
@@ -12,5 +12,7 @@ def append_after(filename="", search_string="", new_string=""):
                 content.insert(lc, new_string)
                 lc += 1
             lc += 1
-        for lines in content:
-            file.write(lines)
+    print(content)
+    with open(filename, 'w', encoding='utf-8') as file:
+        for line in content:
+            file.write(line)
