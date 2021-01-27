765c2a96fc88d0769e23f9c4ec0f2fd07dcc1a91
jfangwang <qbs1864@gmail.com>
2021-01-27 15:06:05 +0000

    fixing docstring

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""base"""
+"""A base file."""
 import json
 
 
@@ -8,7 +8,7 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """init"""
+        """An init function to start."""
         if id is not None:
             self.id = id
         else:
