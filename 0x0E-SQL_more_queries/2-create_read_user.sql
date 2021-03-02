433bc1a30f3f2e5b89df3ed0306ab16d8fac220f
jfangwang <qbs1864@gmail.com>
2021-03-02 18:33:46 +0000

    2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,5 @@
 -- creates database and a user
 CREATE DATABASE IF NOT EXISTS hbtn_0D_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
+GRANT SELECT PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
