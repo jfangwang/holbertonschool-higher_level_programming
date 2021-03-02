0d77e04ba2ce395beff5f301e48cbda06bbb9d18
jfangwang <qbs1864@gmail.com>
2021-03-02 19:14:19 +0000

    7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,6 +1,8 @@
 -- database and table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
-    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    state_id INT NOT NULL FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id),
-    name VARCHAR(256) NOT NULL);
+    id INT NOT NULL AUTO_INCREMENT,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id));
