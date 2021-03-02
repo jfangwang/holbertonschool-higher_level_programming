44eef613a1b27d6b60b9cce83c77e9e636f34f70
jfangwang <qbs1864@gmail.com>
2021-03-02 20:08:57 +0000

    9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,3 @@
 -- lists all cities in database
 SELECT cities.id, cities.name, states.name FROM cities
-LEFT JOIN states ON cities.state_id = states.id
+LEFT JOIN states ON cities.state_id = states.id;
