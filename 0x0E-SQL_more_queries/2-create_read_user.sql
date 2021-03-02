3f785a8ab33e4feb6da479c2085fa5ab03e56fe0
jfangwang <qbs1864@gmail.com>
2021-03-02 18:26:59 +0000

    added privleges

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,5 @@
 -- creates database and a user
 CREATE DATABASE IF NOT EXISTS hbtn_0D_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
