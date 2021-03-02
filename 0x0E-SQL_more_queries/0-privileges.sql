b27ed9f60d17c29777756c302dcdd6ff0c8c996e
jfangwang <qbs1864@gmail.com>
2021-03-02 18:16:21 +0000

    0 updated

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
 -- Show Privilleges for specific users
-SHOW GRANTS FOR user_0d_1@localhost;
-SHOW GRANTS FOR user_0d_2@localhost;
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
