4e490ded00a76a0b1e50b9be43dac3897428566b
jfangwang <qbs1864@gmail.com>
2021-03-03 03:53:41 +0000

    not working 16

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,5 @@
+-- import the database dump
+SELECT tv_shows.title, tv_genres.name FROM tv_genres
+LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+ORDER BY tv_shows.title ASC, tv_genres.name ASC
