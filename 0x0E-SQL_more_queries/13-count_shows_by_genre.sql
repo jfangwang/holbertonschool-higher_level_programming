3a57252400bf14c5b7b0eedc9d882d438009ddc7
jfangwang <qbs1864@gmail.com>
2021-03-03 02:41:01 +0000

    13-

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- import the database dump
-SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows FROM tv_genres
-LEFT JOIN tv_show_genres
+SELECT tv_genres.name AS genre, COUNT(*) AS number_of_tv_shows
+FROM tv_show_genres
+LEFT JOIN tv_genres
 ON tv_show_genres.genre_id = tv_genres.id
-GROUP BY tv_genres.name
-ORDER BY number_of_shows DESC;
+GROUP BY tv_genres.name ORDER BY number_of_tv_shows DESC;
