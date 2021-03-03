ba8e9b8708325a1fd5ff24e68814b70f6663a952
jfangwang <qbs1864@gmail.com>
2021-03-03 04:49:01 +0000

    18

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,8 @@
+-- import the database dump
+SELECT tv_shows.title FROM tv_shows
+WHERE tv_shows.title NOT IN (
+SELECT tv_shows.title FROM tv_genres
+LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_genres.name = "Comedy" ORDER BY tv_shows.title ASC)
+ORDER BY tv_shows.title;
