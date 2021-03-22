f5dc41ad3323e169d22d23e00e0b341e57c70af6
jfangwang <qbs1864@gmail.com>
2021-03-22 16:10:22 +0000

    got rid of code before function

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,14 +1,9 @@
 #!/usr/bin/node
-const argv = process.argv;
-if (isNaN(argv[2])) {
-  console.log(factorial(1, 1));
-} else {
-  console.log(factorial(parseInt(argv[2]), 1));
-}
-function factorial(a, sum) {
-  if (a === 0){
-    return sum
+function factorial(a) {
+  if (a === 0 || isNaN(a)){
+    return 1;
   }
-  sum *= a;
-  return factorial(a - 1, sum);
+  return a * factorial(a - 1);
 }
+let argv = process.argv;
+console.log(factorial(argv[2]));
