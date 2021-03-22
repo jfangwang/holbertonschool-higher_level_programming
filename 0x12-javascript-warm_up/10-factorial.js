8fe3f7f0e0d1a098c55881702f9cecc503bd5159
jfangwang <qbs1864@gmail.com>
2021-03-22 16:11:11 +0000

    semi standard compliant

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-function factorial(a) {
-  if (a === 0 || isNaN(a)){
+function factorial (a) {
+  if (a === 0 || isNaN(a)) {
     return 1;
   }
   return a * factorial(a - 1);
 }
-let argv = process.argv;
+const argv = process.argv;
 console.log(factorial(argv[2]));
