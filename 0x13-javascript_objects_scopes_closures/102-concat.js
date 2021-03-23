8aec212cdc53c5b9c1fca99981bc1e2733fb85f2
jfangwang <qbs1864@gmail.com>
2021-03-23 22:42:54 +0000

    102 done

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
 // Module required to use file selector
 const fs = require('fs');
-const fileA = fs.readFileSync('fileA', 'utf-8');
-const fileB = fs.readFileSync('fileB', 'utf-8');
-for (let a in process.argv) {
-    console.log(a);
+if (process.argv.length === 5) {
+  argv = process.argv;
+  const fileA = fs.readFileSync(argv[2], 'utf-8');
+  const fileB = fs.readFileSync(argv[3], 'utf-8');
+  fs.writeFileSync(argv[4], fileA + '\n' + fileB);
+} else {
+  console.log("USAGE: ./102-concat.js File1 File2 DestFile")
 }
-//fs.writeFileSync('fileC', fileA + '\n' + fileB);
