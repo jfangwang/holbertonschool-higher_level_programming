7337265229a777a6fb93006488fa8fe724e4198d
jfangwang <qbs1864@gmail.com>
2021-03-23 22:45:14 +0000

    102 compliant

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -5,7 +5,7 @@ if (process.argv.length === 5) {
   const argv = process.argv;
   const fileA = fs.readFileSync(argv[2], 'utf-8');
   const fileB = fs.readFileSync(argv[3], 'utf-8');
-  fs.writeFileSync(argv[4], fileA + '\n' + fileB);
+  fs.writeFileSync(argv[4], fileA + fileB);
 } else {
   console.log('USAGE: ./102-concat.js File1 File2 DestFile');
 }
