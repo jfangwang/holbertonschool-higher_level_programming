736bc68bcd9a70e1e5b38a89767d92fd31316d41
jfangwang <qbs1864@gmail.com>
2021-04-08 21:07:32 +0000

    working with recursion and pep8

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,45 +3,32 @@
 
 
 def find_peak(list_of_integers):
-    """ find_peak Time complexity: O(N)"""
+    """ find_peak Time complexity: O(log(n))"""
+    """
+    list_of_integers: An array of numbers that could contain a peak number
+    """
+    middle = int(len(list_of_integers) / 2)
+    # print(str(list_of_integers) + " middle is: " + str(middle))
     if len(list_of_integers) == 0:
         return None
     if len(list_of_integers) == 1:
-        return (list_of_integers[0])
+        return list_of_integers[0]
     if len(list_of_integers) == 2:
         if list_of_integers[0] > list_of_integers[1]:
             return list_of_integers[0]
         else:
+            print(list_of_integers[1])
             return list_of_integers[1]
-    """ list has to have 3 or more numbers """
-    index = 0
-    pre = 0
-    nex = 0
-    cur = 0
-    peak = None
-    while index < len(list_of_integers):
-        try:
-            pre = list_of_integers[index - 1]
-        except:
-            pre = None
-        try:
-            nex = list_of_integers[index + 1]
-        except:
-            nex = None
-        cur = list_of_integers[index]
-        if (pre is not None and nex is not None and
-           (pre <= cur) and (nex <= cur)):
-            if peak is None:
-                peak = cur
-            if cur > peak:
-                peak = cur
-        if pre is None and nex is not None:
-            if cur > nex:
-                peak = cur
-        if nex is None and pre is not None:
-            if peak is None:
-                peak = cur
-            if cur > peak:
-                peak = cur
-        index += 1
-    return str(peak)
+    """Prospects array has to be bigger than 3"""
+
+    if list_of_integers[middle + 1] > list_of_integers[middle]:
+        new_arr = []
+        new_arr = list_of_integers[middle + 1:len(list_of_integers)]
+        return find_peak(new_arr)
+    if list_of_integers[middle - 1] > list_of_integers[middle]:
+        new_arr = []
+        new_arr = list_of_integers[middle - 1:0]
+        return find_peak(new_arr)
+    if (list_of_integers[middle] >= list_of_integers[middle + 1] and
+       list_of_integers[middle] >= list_of_integers[middle + 1]):
+        return list_of_integers[middle]
