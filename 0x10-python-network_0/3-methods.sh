611cf0b66b99ae0e3ef013e1251a37515b0a5ec8
jfangwang <qbs1864@gmail.com>
2021-04-08 18:22:41 +0000

    used sI

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #  displays all HTTP methods the server will accept.
-curl -sD - -o /dev/null "$1" | tail -n 2 | head -n 1 | cut -b 8-
+curl -sI "$1" | tail -n 2 | head -n 1 | cut -b 8-
