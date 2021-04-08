cd759f7907d622e56fdf790d8201c2a556929096
jfangwang <qbs1864@gmail.com>
2021-04-08 22:19:33 +0000

    re-wrote code to 3 simple if statements

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,7 +6,7 @@ def find_peak(list_of_integers):
     """ find_peak Time complexity: O(log(n))"""
     if list_of_integers == []:
         return None
-    return(find(list_of_integers, 0, len(list_of_integers)))
+    return(find(list_of_integers, 0, len(list_of_integers) - 1))
 
 
 def find(num_arr, low, high):
@@ -14,12 +14,9 @@ def find(num_arr, low, high):
     size = high - low
     middle = low + int(size/2)
 
-    if high == low == middle:
-        return num_arr[middle]
-    elif (num_arr[middle] >= num_arr[middle + 1] and
-       num_arr[middle] >= num_arr[middle + 1]):
-        return num_arr[middle]
+    if low == high or middle:
+        return num_arr[low]
     elif num_arr[middle + 1] > num_arr[middle]:
-        return find(num_arr, middle + 1, high - 1)
+        return find(num_arr, middle + 1, high)
     else:
-        return find(num_arr, 0, middle - 1)
+        return find(num_arr, low, middle - 1)
