82202d921a61087b1f32ef4f51fad417aafbf974
jfangwang <qbs1864@gmail.com>
2021-04-08 19:20:59 +0000

    6 task attempt

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" Find a peak """
+
+
+def find_peak(list_of_integers):
+    """ find_peak """
+    if len(list_of_integers) == 0:
+        return None
+    if len(list_of_integers) == 1:
+        return (list_of_integers[0])
+    if len(list_of_integers) == 2:
+        if list_of_integers[0] > list_of_integers[1]:
+            return list_of_integers[0]
+        else:
+            return list_of_integers[1]
+    """ list has to have 3 or more numbers """
+    index = 1
+    pre = 0
+    nex = 0
+    cur = 0
+    peak = None
+    while index < len(list_of_integers) - 1:
+        pre = list_of_integers[index - 1]
+        nex = list_of_integers[index + 1]
+        cur = list_of_integers[index]
+        if ((pre <= cur) and (nex <= cur)):
+            if cur > peak
+            peak = cur
+        index += 1
+    return str(peak)
