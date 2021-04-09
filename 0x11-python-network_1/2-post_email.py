aa684d321b05780b2e6bb5ac1005f90b71467095
jfangwang <qbs1864@gmail.com>
2021-04-09 19:24:29 +0000

    2 testing

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Task 1 """
+import urllib.request
+import urllib.parse
+import sys
+
+if __name__ == "__main__":
+    argv = sys.argv
+    if len(argv) == 3:
+        url = argv[1]
+        email = argv[2]
+        values = {'email': email}
+        data = urllib.parse.urlencode(values)
+        data = data.encode('ascii')
+        req = urllib.request.Request(url, data)
+        with urllib.request.urlopen(url) as response:
+            html = response.read()
+            print(html)
