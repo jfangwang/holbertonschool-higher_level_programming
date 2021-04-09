df926afec628a79bf694832169f67bc0bbadf06f
Jonny Wang <qbs1864@gmail.com>
2021-04-09 17:08:01 -0400

    Update 8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -7,10 +7,14 @@ if __name__ == "__main__":
     q = ""
     if len(argv) == 2:
         url = "http://0.0.0.0:5000/search_user"
+        q = argv[1]
         obj = {'q': q}
         try:
             req = requests.post(url, data=obj).json()
-            print("["+str(req.get('id'))+"] "+str(req.get('name')))
+            if req.get('id') is not None and req.get('name') is not None:
+                print("["+str(req.get('id'))+"] "+str(req.get('name')))
+            else:
+                print("No result")
         except:
             print("Not a valid JSON")
     else:
