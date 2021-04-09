e5e0ba561d2c222786245292099ed8981ceb5ab3
Jonny Wang <qbs1864@gmail.com>
2021-04-09 16:52:44 -0400

    Update 8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -5,23 +5,17 @@ from sys import argv
 
 if __name__ == "__main__":
     q = ""
-        if len(argv) == 2:
-                url = "http://0.0.0.0:5000/search_user"
-                        obj = {'q': q}
-                                req = requests.post(url, obj)
-                                        try:
-                                                    req = requests.post(url, obj)
-                                                                if "id" in req.__dict__ and name in req.__dict__:
-                                                                                print("["+str(req.get('id'))+"]
-                                                                                "+str(req.get('name')))
-                                                                                            else:
-                                                                                                            print("No
-                                                                                                            result")
-                                                                                                                    except:
-                                                                                                                                print("Not
-                                                                                                                                a
-                                                                                                                                valid
-                                                                                                                                JSON")
-                                                                                                                                    else:
-                                                                                                                                            print("No
-                                                                                                                                            result")
+    if len(argv) == 2:
+        url = "http://0.0.0.0:5000/search_user"
+        obj = {'q': q}
+        req = requests.post(url, obj)
+        try:
+            req = requests.post(url, obj)
+            if "id" in req.__dict__ and name in req.__dict__:
+                print("["+str(req.get('id'))+"] "+str(req.get('name')))
+            else:
+                print("No result")
+        except:
+            print("Not a valid JSON")
+    else:
+        print("No result")
