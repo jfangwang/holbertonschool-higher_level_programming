98228084517610a2d401a0fdd79e505cb51a02ad
jfangwang <qbs1864@gmail.com>
2021-05-04 17:54:07 +0000

    2 done

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,6 +2,12 @@
 const url = process.argv[2];
 const request = require('request');
 request(url, function (error, response, body) {
-  console.error('code:', error); // Print the error if one occurred
-  console.log('code:', response && response.statusCode); // Print the response status code if a response was received
-});
+  try{
+    console.log('code:', response && response.statusCode);
+  }
+  catch(err){
+    console.error('code:', error);
+  }
+ 
+}
+);
