aa934fdded17cde8310e880d795093aa0e0ebe95
jfangwang <qbs1864@gmail.com>
2021-05-04 18:50:21 +0000

    4 done

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,12 +2,11 @@
 const url = process.argv[2];
 const request = require('request');
 request(url, function (error, response, body) {
-  try {
-    // console.log('code:', response && response.statusCode);
+  if (error) {
+    console.error(error);
+  } else {
     const obj = JSON.parse(body);
     let count = 0;
-    // console.log(obj.results);
-    // console.log(obj.results);
     for (let a = 0; a < obj.results.length; a++) {
       for (let b = 0; b < obj.results[a].characters.length; b++) {
         if (obj.results[a].characters[b] === 'https://swapi-api.hbtn.io/api/people/18/') {
@@ -16,7 +15,5 @@ request(url, function (error, response, body) {
       }
     }
     console.log(count);
-  } catch (err) {
-    console.error(err);
   }
 });
