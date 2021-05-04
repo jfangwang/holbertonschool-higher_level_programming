0ffebdd85f88faa46e14c4c5b670a7c304bdef91
jfangwang <qbs1864@gmail.com>
2021-05-04 19:34:49 +0000

    6 done

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+const url = process.argv[2];
+const request = require('request');
+const dict = {};
+request(url, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  } else {
+    const obj = JSON.parse(body);
+    let user = obj[0].userId;
+    let TaskCount = 0;
+    for (let a = 0; a < obj.length; a++) {
+      if (obj[a].userId === user) {
+        if (obj[a].completed === true) {
+          TaskCount += 1;
+        }
+      } else {
+        dict[user] = TaskCount;
+        user = obj[a].userId;
+        TaskCount = 0;
+        if (obj[a].userId === user) {
+          if (obj[a].completed === true) {
+            TaskCount += 1;
+          }
+        }
+      }
+      dict[user] = TaskCount;
+    }
+  }
+  console.log(dict);
+});
