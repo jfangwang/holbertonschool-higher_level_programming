8ca0daf88e4eea480197627fdbaf48b0546e0c98
jfangwang <qbs1864@gmail.com>
2021-05-05 15:12:44 +0000

    100

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -12,22 +12,29 @@ const PeepList = ['https://swapi-api.hbtn.io/api/people/',
   'https://swapi-api.hbtn.io/api/people/?page=8',
   'https://swapi-api.hbtn.io/api/people/?page=9'
 ];
-for (const a in PeepList) {
-  url = PeepList[a];
-  request(url, function (error, response, body) {
-    if (error) {
-      console.error(error);
-    } else {
-      const obj = JSON.parse(body);
-      const res = JSON.parse(body).results;
-      url = obj.next;
-      for (let a = 0; a < res.length; a++) {
-        for (const b in res[a].films) {
-          if (res[a].films[b].split('/')[5] === MovieNum) {
-            console.log(res[a].name);
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+async function demo() {
+  for (const a in PeepList) {
+    url = PeepList[a];
+    request(url, function (error, response, body) {
+      if (error) {
+        console.error(error);
+      } else {
+        const obj = JSON.parse(body);
+        const res = JSON.parse(body).results;
+        url = obj.next;
+        for (let a = 0; a < res.length; a++) {
+          for (const b in res[a].films) {
+            if (res[a].films[b].split('/')[5] === MovieNum) {
+              console.log(res[a].name);
+            }
           }
         }
       }
-    }
-  });
+    });
+    await sleep(10);
+  }
 }
+demo();
